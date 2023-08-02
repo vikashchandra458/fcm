7f341c854c7ffa7e2065fa1c66847d5f92e2244b
vikashchandra458 <vikashchandra458@gmail.com>
2023-08-02 22:36:20 +0530

    Firebase Console Messaging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
+import React, { useState, useEffect } from 'react'
+import toast, { Toaster, ToastBar } from 'react-hot-toast';
+import { messaging } from "./firebase"
+import { getToken, onMessage } from 'firebase/messaging';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [notification, setNotification] = useState({ title: '', body: '' });
+  const notify = () => toast(<ToastDisplay />);
+  function ToastDisplay() {
+    return (
+      <div>
+        <p><b>{notification?.title}</b></p>
+        <p>{notification?.body}</p>
+      </div>
+    );
+  };
+
+  const requestForToken = () => {
+    return getToken(messaging, { vapidKey: "BOE-MkGX6wlCDTGQibU5F5S7qwxoFVr3mAOf6gkAH6noQQf0g8tMMBSm5A7kf0ncPJdtXnlck1UTn0cxoLhEbI0" })
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log('current token for client: ', currentToken);
+          // Perform any other neccessary action with the token
+        } else {
+          // Show permission request UI
+          console.log('No registration token available. Request permission to generate one.');
+        }
+      })
+      .catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+      });
+  };
+
+  const onMessageListener = () =>
+    new Promise((resolve) => {
+      onMessage(messaging, (payload) => {
+        console.log("payload", payload)
+        resolve(payload);
+      });
+    });
+
+  useEffect(() => {
+    if (notification?.title) {
+      notify()
+    }
+  }, [notification])
+
+  requestForToken();
+
+  onMessageListener()
+    .then((payload) => {
+      setNotification({ title: payload?.notification?.title, body: payload?.notification?.body });
+    })
+    .catch((err) => console.log('failed: ', err));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Toaster>
+        {(t) => (
+          <ToastBar
+            toast={t}
+            style={{
+              ...t.style,
+              animation: t.visible ? 'custom-enter 1s ease' : 'custom-exit 1s ease',
+            }}
+          />
+        )}
+      </Toaster>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
+import { getMessaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1svoHkoM1ccWJ7cDppkHCpDEBOCNNth4",
-    authDomain: "v-chat-8ac16.firebaseapp.com",
-    projectId: "v-chat-8ac16",
-    storageBucket: "v-chat-8ac16.appspot.com",
-    messagingSenderId: "514723356205",
-    appId: "1:514723356205:web:246edf2ec0d37bbb5ee2a4"
-  };
+  apiKey: "AIzaSyD1svoHkoM1ccWJ7cDppkHCpDEBOCNNth4",
+  authDomain: "v-chat-8ac16.firebaseapp.com",
+  projectId: "v-chat-8ac16",
+  storageBucket: "v-chat-8ac16.appspot.com",
+  messagingSenderId: "514723356205",
+  appId: "1:514723356205:web:246edf2ec0d37bbb5ee2a4"
+};
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
