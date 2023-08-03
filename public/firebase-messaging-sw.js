2c45bfe313d1f3035d75eab1b892fa53eab83e8e
vikashchandra458 <vikashchandra458@gmail.com>
2023-08-03 14:43:38 +0530

    reverted

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,23 +4,22 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "AIzaSyBpDd67ng-pksBxKjKKGhZ1rXjl_Fk9NC0",
-  authDomain: "xonfi-6c328.firebaseapp.com",
-  projectId: "xonfi-6c328",
-  storageBucket: "xonfi-6c328.appspot.com",
-  messagingSenderId: "93149356759",
-  appId: "1:93149356759:web:2d5e3dfdb549fedc020692",
-  measurementId: "G-1ECWSCHELX"
-};
+    apiKey: "AIzaSyD1svoHkoM1ccWJ7cDppkHCpDEBOCNNth4",
+    authDomain: "v-chat-8ac16.firebaseapp.com",
+    projectId: "v-chat-8ac16",
+    storageBucket: "v-chat-8ac16.appspot.com",
+    messagingSenderId: "514723356205",
+    appId: "1:514723356205:web:246edf2ec0d37bbb5ee2a4"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
+messaging.onBackgroundMessage(function(payload) {
   console.log('Received background message ', payload);
-  // Customize notification here
+ // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
